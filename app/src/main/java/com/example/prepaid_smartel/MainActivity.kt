48fdc6bb50c48d05d7aa89d0e387c08c0f7ae4df
Bansj <bsj98@smartel.com>
2023-05-08 09:03:00 +0900

    Api JSON형태로 연결 성공

--- app/src/main/java/com/example/prepaid_smartel/MainActivity.kt
+++ app/src/main/java/com/example/prepaid_smartel/MainActivity.kt
@@ -1,28 +1,94 @@
 package com.example.prepaid_smartel
 
+
+
+
+import android.app.ProgressDialog
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var phoneNumberEditText: EditText
+    private lateinit var phoneInput: EditText
+    private lateinit var searchButton: Button
+
+    private lateinit var loadingSpinner: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        phoneNumberEditText = findViewById(R.id.editTxt_phoneNums)
-        val inquiryButton = findViewById<Button>(R.id.btn_search)
+        // Get references to UI elements
+        phoneInput = findViewById(R.id.btn_editText)
+        searchButton = findViewById(R.id.btn_search)
 
-        inquiryButton.setOnClickListener {
-            val phoneNumber = phoneNumberEditText.text.toString()
-            val intent = Intent(this, PrepaidInfoActivity::class.java)
-            intent.putExtra("phoneNumber", phoneNumber)
-            startActivity(intent)
+        loadingSpinner = findViewById(R.id.loading_spinner)
+
+        // Set up click listener for search button
+        searchButton.setOnClickListener {
+            val phoneNumber = phoneInput.text.toString()
+            fetchPrepaidInfo(phoneNumber)
         }
+    }
+
+
+
+    private fun fetchPrepaidInfo(phoneNumber: String) {
+        val url = "http://61.41.9.34/lg_RealTime/api_prepaid_info.php"
+
+        // Create JSON object with phone number parameter
+        val jsonObject = JSONObject()
+        jsonObject.put("hp_no", phoneNumber)
+
+        // Show loading spinner
+        loadingSpinner.visibility = View.VISIBLE
+
+        // Make request to API with Volley library
+        val request = JsonObjectRequest(Request.Method.POST, url, jsonObject,
+            { response ->
+                // On successful response, log each item and start PrepaidInfoActivity
+                Log.d("PrepaidInfo", "Response: $response")
+                val carrier = response.getString("carrier")
+                Log.d("PrepaidInfo", "Carrier: $carrier")
+                val rateNm = response.getString("rateNm")
+                Log.d("PrepaidInfo", "Rate name: $rateNm")
+                val rateAmt = response.getString("rate_amt")
+                Log.d("PrepaidInfo", "Rate amount: $rateAmt")
+                val remain = response.getString("remain")
+                Log.d("PrepaidInfo", "Remain: $remain")
+                val bank = response.getString("bank")
+                Log.d("PrepaidInfo", "Bank: $bank")
+                val bankAcnt = response.getString("bank_acnt")
+                Log.d("PrepaidInfo", "Bank account: $bankAcnt")
+                val intent = Intent(this, PrepaidInfoActivity::class.java)
+                intent.putExtra("carrier", carrier)
+                intent.putExtra("rateNm", rateNm)
+                intent.putExtra("rateAmt", rateAmt)
+                intent.putExtra("remain", remain)
+                intent.putExtra("bank", bank)
+                intent.putExtra("bankAcnt", bankAcnt)
+                startActivity(intent)
+            },
+            { error ->
+                // On error, show error message
+                Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                Log.e("PrepaidInfo", "Error: ${error.message}")
+            }
+        )
+
+        // Add request to Volley queue
+        Volley.newRequestQueue(this).add(request)
     }
 }
--- app/src/main/java/com/example/prepaid_smartel/PrepaidInfoActivity.kt
+++ app/src/main/java/com/example/prepaid_smartel/PrepaidInfoActivity.kt
@@ -1,78 +1,44 @@
 package com.example.prepaid_smartel
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
 
 class PrepaidInfoActivity : AppCompatActivity() {
-    private lateinit var numberTextView: TextView
-    private lateinit var carrierTextView: TextView
-    private lateinit var ratePlanTextView: TextView
-    private lateinit var rateAmountTextView: TextView
-    private lateinit var remainTextView: TextView
-    private lateinit var bankTextView: TextView
-    private lateinit var bankAccountTextView: TextView
+
+    private lateinit var carrierText: TextView
+    private lateinit var rateNameText: TextView
+    private lateinit var rateAmountText: TextView
+    private lateinit var remainingText: TextView
+    private lateinit var bankText: TextView
+    private lateinit var bankAccountText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prepaid_info)
 
-        numberTextView = findViewById(R.id.result_nums)
-        carrierTextView = findViewById(R.id.result_carrier)
-        ratePlanTextView = findViewById(R.id.result_ratePlan)
-        rateAmountTextView = findViewById(R.id.result_rateAmount)
-        remainTextView = findViewById(R.id.result_remain)
-        bankTextView = findViewById(R.id.result_bank)
-        bankAccountTextView = findViewById(R.id.result_bankAccount)
+        // Get references to UI elements
+        carrierText = findViewById(R.id.result_carrier)
+        rateNameText = findViewById(R.id.result_ratePlan)
+        rateAmountText = findViewById(R.id.result_rateAmount)
+        remainingText = findViewById(R.id.result_remain)
+        bankText = findViewById(R.id.result_bank)
+        bankAccountText = findViewById(R.id.result_bankAccount)
 
-        val number = intent.getStringExtra("number")
-
-        // Call AsyncTask to retrieve prepaid information using the API
-        val prepaidInfoTask = PrepaidInfoTask()
-        prepaidInfoTask.execute(number)
+        // Get data from intent and display in UI
+        val carrier = intent.getStringExtra("carrier")
+        carrierText.text = "Carrier: $carrier"
+        val rateNm = intent.getStringExtra("rateNm")
+        rateNameText.text = "Rate name: $rateNm"
+        val rateAmt = intent.getStringExtra("rateAmt")
+        rateAmountText.text = "Rate amount: $rateAmt"
+        val remain = intent.getStringExtra("remain")
+        remainingText.text = "Remaining data: $remain"
+        val bank = intent.getStringExtra("bank")
+        bankText.text = "Bank: $bank"
+        val bankAcnt = intent.getStringExtra("bankAcnt")
+        bankAccountText.text = "Bank account: $bankAcnt"
     }
 
-    inner class PrepaidInfoTask : AsyncTask<String, Void, JSONObject>() {
-        override fun doInBackground(vararg params: String?): JSONObject? {
-            val number = params[0]
-            val url = "http://61.41.9.34/lg_RealTime/api_prepaid_info.php?hp_no=$number"
-
-            val connection = URL(url).openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
-
-            val inputStream = connection.inputStream
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-            val response = StringBuilder()
-
-            bufferedReader.use {
-                var line = it.readLine()
-                while (line != null) {
-                    response.append(line)
-                    line = it.readLine()
-                }
-            }
-
-            return JSONObject(response.toString())
-        }
-
-        override fun onPostExecute(result: JSONObject?) {
-            super.onPostExecute(result)
-            if (result != null) {
-                numberTextView.text = result.getString("hp_no")
-                carrierTextView.text = result.getString("carrier")
-                ratePlanTextView.text = result.getString("rateNm")
-                rateAmountTextView.text = result.getString("rate_amt")
-                remainTextView.text = result.getString("remain")
-                bankTextView.text = result.getString("bank")
-                bankAccountTextView.text = result.getString("bank_acnt")
-            }
-        }
-    }
 }
 
